775f19b43283c7b0baa458943e80cebb323b0e2d
Ryan Schneider <slcryan2010@yahoo.com>
2023-12-14 16:44:28 -0700

    student setup

---
+++ students.js
@@ -0,0 +1,134 @@
+const possibleClasses = [
+    'Math', 'English', 'Physics', 'History', 'Chemistry',
+    'Biology', 'Computer Science', 'Psychology', 'Music', 'Art'
+  ];
+  
+  const students = [
+    {
+      firstName: 'Emma',
+      lastName: 'Johnson',
+      scores: [92, 78, 81, 76, 82],
+      classes: ['Physics', 'History', 'Biology', 'Music', 'Psychology']
+    },
+    {
+      firstName: 'Liam',
+      lastName: 'Smith',
+      scores: [90, 97, 85, 89, 94],
+      classes: ['English', 'Physics', 'Chemistry', 'Psychology', 'Art']
+    },
+    {
+      firstName: 'Olivia',
+      lastName: 'Davis',
+      scores: [87, 76, 72, 80, 71],
+      classes: ['History', 'Chemistry', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Noah',
+      lastName: 'Taylor',
+      scores: [80, 85, 79, 50, 77],
+      classes: ['Physics', 'Computer Science', 'Music', 'History', 'Art']
+    },
+    {
+      firstName: 'Ava',
+      lastName: 'Williams',
+      scores: [99, 95, 93, 88, 91],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Sophia',
+      lastName: 'Brown',
+      scores: [81, 50, 78, 77, 83],
+      classes: ['History', 'Computer Science', 'Chemistry', 'Music', 'Biology']
+    },
+    {
+      firstName: 'Jackson',
+      lastName: 'Clark',
+      scores: [75, 82, 78, 69, 83],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Art']
+    },
+    {
+      firstName: 'Oliver',
+      lastName: 'Miller',
+      scores: [93, 76, 79, 75, 81],
+      classes: ['Math', 'English', 'Physics', 'History', 'Art']
+    },
+    {
+      firstName: 'Mia',
+      lastName: 'Moore',
+      scores: [50, 81, 77, 79, 75],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Lucas',
+      lastName: 'Turner',
+      scores: [77, 74, 80, 82, 78],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Art']
+    },
+    {
+      firstName: 'Harper',
+      lastName: 'Ward',
+      scores: [80, 78, 76, 74, 82],
+      classes: ['Math', 'English', 'Physics', 'History', 'Art']
+    },
+    {
+      firstName: 'Ethan',
+      lastName: 'Baker',
+      scores: [72, 79, 85, 71, 76],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Aria',
+      lastName: 'Fisher',
+      scores: [94, 73, 76, 80, 77],
+      classes: ['History', 'Chemistry', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Logan',
+      lastName: 'Hill',
+      scores: [78, 90, 74, 77, 81],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Art']
+    },
+    {
+      firstName: 'Ava',
+      lastName: 'Williams',
+      scores: [76, 85, 72, 78, 79],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Elijah',
+      lastName: 'Walker',
+      scores: [100, 95, 75, 73, 80],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Art']
+    },
+    {
+      firstName: 'Aiden',
+      lastName: 'Evans',
+      scores: [75, 82, 78, 76, 83],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Carter',
+      lastName: 'Adams',
+      scores: [83, 76, 79, 75, 81],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Art']
+    },
+    {
+      firstName: 'Scarlett',
+      lastName: 'Cox',
+      scores: [74, 81, 77, 79, 75],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Music']
+    },
+    {
+      firstName: 'Thomas',
+      lastName: 'Parker',
+      scores: [77, 74, 80, 82, 78],
+      classes: ['Physics', 'History', 'Computer Science', 'Psychology', 'Art']
+    },
+  ];
+  
+//   console.log(students);
+
+module.exports = {
+    students,
+    possibleClasses
+}
